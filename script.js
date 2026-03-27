a7180f120146eefbd70ef0970a216e98fb17e3b7
renznathanael0-code <renznathanael0@gmail.com>
2026-03-27 20:42:55 +0100

    lautstärke höher

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ async function initMic() {
             let average = values / dataArray.length;
 
             // Lautstärke-Schwelle fürs Pusten
-            if (average > 55) { 
+            if (average > 70) { 
                 flipCard();
             }
             requestAnimationFrame(checkVolume);
